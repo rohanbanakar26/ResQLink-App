b239c102d04b7247764a321a622dadf3b4792c2b
Rohan Prakash Banakar <rohanprakashbanakar@Rohans-MacBook-Air.local>
2026-04-04 19:03:07 +0530

    Automation: Triggering new build to incorporate Secrets

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,6 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+// Triggering build with secrets
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
